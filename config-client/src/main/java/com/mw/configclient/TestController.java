1e8654c1ed85d2fba69f96f3abe67a171f633d51
zz <zz>
2020-10-30 21:23:56 +0800

    config

---
+++ config-client/src/main/java/com/mw/configclient/TestController.java
@@ -0,0 +1,18 @@
+package com.mw.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
+}
